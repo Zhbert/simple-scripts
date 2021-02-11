aafa57569f465af8f7fc4c61773221ea4b79c3b3
Zhbert <zhbert@yandex.ru>
2021-02-11 21:49:36 +0300

    Some new code (2)

--- manga-downloader/main.py
+++ manga-downloader/main.py
@@ -3,8 +3,21 @@
 #  Licensed by GPLv3.
 
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import requests as req
 import os
+
+
+def scan_chapter(link):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    browser = webdriver.Chrome(chrome_options=options)
+    browser.get(link)
+    print(browser.find_element_by_id('mangaPicture').get_attribute('src'))
+    generated_html = browser.page_source
+    browser.quit()
+    file = open('html', 'w')
+    file.write(generated_html)
 
 
 def clean_name(text):
@@ -24,10 +37,11 @@ def get_chapter_links(url):
     hrefs = soup.find('table', class_='table table-hover').find_all('a')
     file = open('chaptesLinks.txt', 'w')
     for href in hrefs:
-        file.write('https://mintmanga.live' + href.get('href') + 'mtr=1' + '\n')
+        file.write('https://mintmanga.live' + href.get('href') + '?mtr=1' + '\n')
     file.close()
 
 
 if __name__ == '__main__':
     get_chapter_links(
         'https://mintmanga.live/mag_celitel__novyi_start___vysshee_iscelenie__chary_momentalnoi_smerti_i_kraja_umenii')
+    scan_chapter('https://mintmanga.live/mag_celitel__novyi_start___vysshee_iscelenie__chary_momentalnoi_smerti_i_kraja_umenii/vol8/64?mtr=1')
