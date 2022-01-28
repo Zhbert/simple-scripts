b77db947ab89bf2c0babe7d7f846c74090c9359e
Zhbert <zhbert@yandex.ru>
2022-01-28 21:09:35 +0300

    Add settings file service

---
+++ raufus_checker/main.py
@@ -0,0 +1,10 @@
+#  -*- coding: utf-8 -*-
+#
+#  Copyright (c) 2022.
+#  Created by Zhbert.
+#  Licensed by GPLv3.
+
+from settings_file_service import *
+
+if __name__ == '__main__':
+    check_settings_file()
---
+++ raufus_checker/settings_file_service.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2022.
+#  Created by Zhbert.
+#  Licensed by GPLv3.
+
+import os
+import configparser
+
+
+def check_settings_file():
+    settings_path = get_home_path() + os.sep + ".raufus_checker"
+    settings_file = settings_path + os.sep + "raufus_checker.conf"
+    if os.path.exists(settings_path):
+        print("The settings directory was found. Checking the parameters file...")
+        if os.path.exists(settings_file):
+            print("The settings file was found.")
+        else:
+            print("Creating a new settings file...")
+            create_new_settings_file(settings_file)
+            print("A new settings file has been created")
+    else:
+        print("The settings directory was not found: " + settings_path)
+        print("Creating a settings directory:" + settings_path)
+        os.mkdir(settings_path)
+        create_new_settings_file(settings_file)
+
+
+def create_new_settings_file(settings_file):
+    print("The settings file was not found. Creating...")
+    config = configparser.ConfigParser()
+    config.add_section("CHILD")
+    config.set("CHILD", "Surname", "NONE")
+    config.set("CHILD", "Name", "NONE")
+    config.set("CHILD", "Middle_name", "NONE")
+    if os.path.exists(settings_file):
+        with open(settings_file, "w") as config_file:
+            config.write(config_file)
+            config_file.close()
+    else:
+        with open(settings_file, "x") as config_file:
+            config.write(config_file)
+            config_file.close()
+
+
+def get_home_path():
+    home_path = (os.getenv('HOME'))
+    if home_path == "None":
+        home_path = os.getenv('USERPROFILE')
+    return home_path
+
+
+def get_settings_file_path():
+    return get_home_path() + os.sep + ".raufus_checker" + os.sep + "raufus_checker.conf"
